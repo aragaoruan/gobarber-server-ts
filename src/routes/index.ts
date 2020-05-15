76fa9ab03ba38711b5b3a3b0d9e0e4bb89019792
Ruan Aragao <aragao.ruan@gmail.com>
2020-05-15 15:47:24 -0300

    chore: debug

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,16 @@
 import { Router } from 'express';
 
 const routes = Router();
+routes.post('/user', (request, response) => {
+  const { name, email } = request.body;
+
+  const user = {
+    name,
+    email,
+  };
+  return response.json(user);
+});
+
 routes.get('/', (request, response) =>
   response.json({ message: 'Hello World' }),
 );
