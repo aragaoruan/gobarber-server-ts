d3bb6cf45e8167ec1b32b05b722ef236ad555b70
Ruan Aragao <aragao.ruan@gmail.com>
2020-05-20 12:05:24 -0300

    feat: persistindo no banco de dados agendamento

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,29 +1,29 @@
 import { Router } from 'express';
+import { getCustomRepository } from 'typeorm';
 import { parseISO } from 'date-fns';
 
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
 import CreateAppointmentService from '../services/CreateAppointmentService';
 
 const route = Router();
-const appointmentsRepository = new AppointmentsRepository();
 
-route.get('/', (request, response) => {
-  const appointments = appointmentsRepository.all();
+route.get('/', async (request, response) => {
+  const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+
+  const appointments = await appointmentsRepository.find();
 
   return response.json(appointments);
 });
 
-route.post('/', (request, response) => {
+route.post('/', async (request, response) => {
   try {
     const { provider, date } = request.body;
 
     const parsedDate = parseISO(date);
 
-    const createAppointmentService = new CreateAppointmentService(
-      appointmentsRepository,
-    );
+    const createAppointmentService = new CreateAppointmentService();
 
-    const appointment = createAppointmentService.execute({
+    const appointment = await createAppointmentService.execute({
       date: parsedDate,
       provider,
     });
