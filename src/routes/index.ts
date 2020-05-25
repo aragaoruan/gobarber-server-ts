d9a027fd58a99739b344c27c5511cbfd04ba2348
Ruan Aragao <aragao.ruan@gmail.com>
2020-05-25 16:35:16 -0300

    feat: gerando JWT

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import appointmentsRouter from './appointments.routes';
 import UsersRouter from './users.routes';
+import SessionRouter from './session.routes';
 
 const routes = Router();
 
 routes.use('/appointments', appointmentsRouter);
 routes.use('/users', UsersRouter);
+routes.use('/sessions', SessionRouter);
 
 export default routes;
---
+++ src/services/AuthenticateUserService.ts
@@ -0,0 +1,46 @@
+import { getRepository } from 'typeorm';
+import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
+
+import User from '../models/User';
+
+interface Request {
+  email: string;
+  password: string;
+}
+interface Response {
+  user: User;
+  token: string;
+}
+
+class AuthenticateUserService {
+  public async execute({ email, password }: Request): Promise<Response> {
+    const userRepository = getRepository(User);
+
+    const user = await userRepository.findOne({
+      where: { email },
+    });
+
+    if (!user) {
+      throw new Error('Incorrect email/password combination');
+    }
+
+    const passwordMatched = await compare(password, user.password);
+
+    if (!passwordMatched) {
+      throw new Error('Incorrect email/password combination');
+    }
+
+    const token = sign({}, 'b1f952434478cdf472c8ef1c87dd7569', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
+    return {
+      user,
+      token,
+    };
+  }
+}
+
+export default AuthenticateUserService;
