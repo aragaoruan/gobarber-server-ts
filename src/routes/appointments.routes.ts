5260615ce3e8d65543151552e111df3436b38c8c
Ruan Aragao <aragao.ruan@gmail.com>
2020-05-26 15:05:43 -0300

    Tratativa de erros

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -20,22 +20,18 @@ route.get('/', async (request, response) => {
 });
 
 route.post('/', async (request, response) => {
-  try {
-    const { provider_id, date } = request.body;
+  const { provider_id, date } = request.body;
 
-    const parsedDate = parseISO(date);
+  const parsedDate = parseISO(date);
 
-    const createAppointmentService = new CreateAppointmentService();
+  const createAppointmentService = new CreateAppointmentService();
 
-    const appointment = await createAppointmentService.execute({
-      date: parsedDate,
-      provider_id,
-    });
+  const appointment = await createAppointmentService.execute({
+    date: parsedDate,
+    provider_id,
+  });
 
-    return response.json(appointment);
-  } catch (err) {
-    return response.status(400).json({ error: err.message });
-  }
+  return response.json(appointment);
 });
 
 export default route;
